c8f1060ba2a307fc12d78d57623545ce59081b2e
batyarimskiy <70210154+batyarimskiy@users.noreply.github.com>
2020-08-30 12:38:46 +0300

    Update dfphone.py

--- dfphone.py
+++ dfphone.py
@@ -78,38 +78,7 @@ class AESCipher(object):
     def encrypt(self, raw):
         raw = self._pad(raw)
         cipher = AES.new(self.AES_KEY, AES.MODE_ECB)
-        return base64.b64encode(cipher.encrypt(raw.encode()))
-
-    def decrypt(self, enc):
-        enc = base64.b64decode(enc)
-        cipher = AES.new(self.AES_KEY, AES.MODE_ECB)
-        return self._unpad(cipher.decrypt(enc)).decode('utf-8')
-
-    def _pad(self, s):
-        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
-
-    @staticmethod
-    def _unpad(s):
-        return s[:-ord(s[len(s)-1:])]
-
-
-aes = AESCipher(AES_KEY)
-
-def sendPost(url, data, sig, ts):
-
-    headers = {'X-App-Version': '4.9.1',
-        'X-Token':TOKEN,
-        'X-Os': 'android 5.0',
-        'X-Client-Device-Id': '14130e29cebe9c39',
-        'Content-Type': 'application/json; charset=utf-8',
-        'Accept-Encoding': 'deflate',
-        'X-Req-Timestamp': ts,
-        'X-Req-Signature': sig,
-        'X-Encrypted': '1'}
-    r = requests.post(url, data=data, headers=headers, verify=True)
-    return json.loads(aes.decrypt(r.json()['data']))
-
-def getByPhone(phone):
+        return base64.b64encode(cipher.encryp
 
     ts = str(int(time.time()))
 
@@ -148,35 +117,7 @@ def pars_getcontact():
         print(colored(""" | |_| |  __/ ||_____| |__| (_) | | | | || (_| | (__| |_  """, "white"))
         print(colored("""  \____|\___|\__|     \____\___/|_| |_|\__\__,_|\___|\__| """, "white"))
         print(colored("""                                                          """, "white"))
-        phone = input(colored("Номер с '+' : ", 'white'))
-
-        if '+' not in phone:
-                phone = '+'+phone
-
-        print(colored('\n========GETCONTACT========', 'blue'))
-        finfo = getByPhone(phone)
-
-        try:
-                if finfo['result']['profile']['displayName']:
-                        print(colored(finfo['result']['profile']['displayName'], 'green'))
-                        print(colored('Имен найдено: '+str(finfo['result']['profile']['tagCount']), 'yellow'))
-
-                        try:
-                                print('\n'.join([i['tag'] for i in getByPhoneTags(phone)['result']['tags']]))
-                        except KeyError:
-                                if finfo['result']['profile']['tagCount'] > 0:
-                                        print('Имя найдено, но для просморта нужен премиум')
-                                        print('Телеграм - @BatyaRimskiy1')
-                                else:
-                                        pass
-                else:
-                        print('Не найдено!')
-        except:
-                print('Не найдено!')
-
-
-def pars_avito():
-
+      
         print(colored("""     _        _ _         """, "white"))
         print(colored("""    / \__   _(_) |_ ___   """, "white"))
         print(colored("""   / _ \ \ / / | __/ _ \  """, "white"))
@@ -185,29 +126,6 @@ def pars_avito():
         print(colored("""                          """, "white"))
         phone = input(colored("Номер с '+' : ", 'white'))
         os.system("clear")
-
-        page = requests.get('https://mirror.bullshit.agency/search_by_phone/'+phone)
-        soup = BeautifulSoup(page.text, 'html.parser')
-
-        print(colored("========PARSER AVITO=======", 'white'))
-        print("\n")
-
-        #tag html search and class = name
-        classsell=soup.find(class_='media-heading')
-        namesell= soup.find_all('h4')
-
-        #for tag = name
-        for classsell in namesell:
-                print(colored("Название обьявления: ", 'green'), classsell.text)
-
-        #tag html search and class = adresat
-        classtext = soup.find(class_='text-muted')
-        nametext = soup.find_all('span')
-
-        #for tag = adresat
-        for classtext in nametext:
-                print(colored("Найден адрес и дата\n", 'green'), classtext.text)
-
 def pars_simcard():
         print(colored("""  ____ ___ __  __        ____              _  """, "white"))
         print(colored(""" / ___|_ _|  \/  |      / ___|__ _ _ __ __| | """, "white"))
@@ -217,17 +135,3 @@ def pars_simcard():
         print(colored("""                                              """, "white"))
         phone = input(colored("Номер c '+': ", 'white'))
         try:
-                getInfo = "https://htmlweb.ru/geo/api.php?json&telcod=" + str(phone)
-                try:
-                        infoPhone = urllib.request.urlopen( getInfo )
-                except:
-                        print( "\nТелефон не найден\n" )
-                infoPhone = json.load( infoPhone )
-                print( u"Страна:", infoPhone["country"]["name"] )
-                print( u"Регион:", infoPhone["region"]["name"] )
-                print( u"Округ:", infoPhone["region"]["okrug"] )
-                print( u"Оператор:", infoPhone["0"]["oper"] )
-                print( u"Часть света:", infoPhone["country"]["location"] )
-        except:
-                print(colored("Не найдено!", "red"))
-main_menu()
